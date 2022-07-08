bf6086016990662a9fb5e48790e58d6eab6271a4
Kirill Chetverikov <MaraZmOo@yandex.ru>
2022-07-08 22:38:47 +0300

    Добавил параметризацию к структурам данных

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+      Stack<String> stack = new Stack<>();
+
+      stack.push("aaaa");
+      stack.push("bbbb");
+      stack.push("cccc");
+
+      stack.pop();
+
+      stack.peek();
+
+      stack.print();
+    }
+}
